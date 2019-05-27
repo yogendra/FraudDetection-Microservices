9a1de481328e85ed222abf4e928c2863b5d02d30
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-05-28 00:33:15 +0800

    making startup gfsh dynamic

--- GemfireServer/startup.sh
+++ GemfireServer/startup.sh
@@ -1,1 +1,7 @@
 $GEODE_HOME/bin/gfsh run --file startup.gfsh
+
+
+
+export CFDEV_HOST=$(dig +short  host.dev.cfdev.sh)
+cat startup.gfsh.tmpl | sed "s/\$CFDEV_HOST/$CFDEV_HOST/g" > startup.gfsh
+$GEODE_HOME/bin/gfsh run --file startup.gfsh
