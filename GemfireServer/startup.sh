e3416189a9445ecc2057e7004ac9330d9c5d1bd7
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-05-28 00:51:46 +0800

    fixing ip

--- GemfireServer/startup.sh
+++ GemfireServer/startup.sh
@@ -1,7 +1,5 @@
-$GEODE_HOME/bin/gfsh run --file startup.gfsh
 
-
-
-export CFDEV_HOST=$(dig +short  host.dev.cfdev.sh)
+[[ $# -lt 1 ]] && echo "No external hostnamem provided" && exit 1
+export CFDEV_HOST=$1
 cat startup.gfsh.tmpl | sed "s/\$CFDEV_HOST/$CFDEV_HOST/g" > startup.gfsh
 $GEODE_HOME/bin/gfsh run --file startup.gfsh
