b2b1dbbc0d813e2bb9bd7d4f6af5f7f11ce9a656
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-05-28 17:34:47 +0800

    added rabiit

--- ClusteringService/cf-createservice.sh
+++ ClusteringService/cf-createservice.sh
@@ -1,2 +1,3 @@
 cf cups gpdb -p '{"URL":"jdbc:postgresql://host.dev.cfdev.sh:5432/gemfire?user=pivotal&password=pivotal"}'
 cf create-service p.redis cache-small redis 
+cf create-service p-rabbitmq standard rabbit
