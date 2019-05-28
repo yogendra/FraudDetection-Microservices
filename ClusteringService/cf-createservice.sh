fdb139e5b2f2ddaf1d45643518e7913beccea34e
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-05-28 17:32:18 +0800

    fixed service type

--- ClusteringService/cf-createservice.sh
+++ ClusteringService/cf-createservice.sh
@@ -1,2 +1,2 @@
-cf cups gpdb -p '{"URL":"jdbc:postgresql://192.168.11.1:5432/gemfire?user=pivotal&password=pivotal"}'
-cf create-service p-redis shared-vm redis 
+cf cups gpdb -p '{"URL":"jdbc:postgresql://host.dev.cfdev.sh:5432/gemfire?user=pivotal&password=pivotal"}'
+cf create-service p.redis cache-small redis 
--- TransactionsConsole/cf-createservice.txt
+++ TransactionsConsole/cf-createservice.txt
@@ -1,1 +1,1 @@
-cf cups gemfire -p '{"locatorHost":"192.168.11.1","locatorPort":"10334", "RestEndpoint":"http://192.168.11.1:8888/gemfire-api/v1/"}'
+cf cups gemfire -p '{"locatorHost":"host.dev.cfdev.sh","locatorPort":"10334", "RestEndpoint":"http://host.dev.cfdev.sh:8888/gemfire-api/v1/"}'
