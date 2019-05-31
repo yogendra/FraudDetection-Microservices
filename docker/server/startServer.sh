d80a16560fc833ef686bfb2d4b66e8a61f8ee24f
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-05-31 20:54:08 +0800

    Overhauled docker services

--- docker/server/startServer.sh
+++ docker/server/startServer.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-
-
 mkdir -p /data/$HOSTNAME
 
-gfsh start server --name=$HOSTNAME --locators=locator[10334] --dir=/data/$HOSTNAME/ --server-port=40404 --max-heap=1G --cache-xml-file=/server-cache.xml --J=-Dgemfire.start-dev-rest-api=true --J=-Dgemfire.http-service-port=8888
+gfsh \
+  start server \
+  --name=$HOSTNAME \
+  --locators=$LOCATORS \
+  --locator-wait-time=300 \
+  --jmx-manager-hostname-for-clients=${PUBLIC_IP} \
+  --hostname-for-clients=${PUBLIC_IP} \
+  --dir=/data/$HOSTNAME/ \
+  --server-port=40404 \
+  --max-heap=1G \
+  --cache-xml-file=/server-cache.xml \
+  --start-rest-api \
+  --http-service-port=8888
 
 while true; do
   sleep 60
--- docker/start-docker-machine.sh
+++ docker/start-docker-machine.sh
@@ -2,7 +2,11 @@
 
 # export GOOGLE_PROJECT=pa-yrampuria
 # export GOOGLE_ZONE=asia-southeast1-a
+SCRIPT_ROOT=$(cd `dirname $0`; pwd)
 
+
+
+export DOCKER_MACHINE_NAME=fds
 
 docker-machine create \
   --driver google \
@@ -12,14 +16,17 @@ docker-machine create \
   --google-open-port 8888/tcp \
   --google-open-port 40404/tcp \
   --google-open-port 7575/tcp \
+  --google-open-port 1099/tcp \
+  --google-open-port 10334/tcp \
   $* \
-  gp-services
+  $DOCKER_MACHINE_NAME
 echo "Machine Created Successfully"  
-eval $(docker-machine env gp-services)
+export DOCKER_MACHINE_IP=$(docker-machine ip $DOCKER_MACHINE_NAME)
+eval $(docker-machine env $DOCKER_MACHINE_NAME)
 
 echo "Running gemfire and postgredb services"
-docker-compose up -d
-DOCKER_MACHINE_IP=$(docker-machine ip gp-services)
+docker-compose -f $SCRIPT_ROOT/docker-compose.yml up -d
+
 
 cat <<EOF
 Service up and running. 
@@ -29,4 +36,8 @@ You can now register gemfire and postgres as CUPS on PCF with following commands
 cf cups gpdb -p '{"URL":"jdbc:postgresql://$DOCKER_MACHINE_IP:5432/gemfire?user=pivotal&password=pivotal"}'
 
 cf cups gemfire -p '{"locatorHost":"$DOCKER_MACHINE_IP","locatorPort":"10334", "RestEndpoint":"http://$DOCKER_MACHINE_IP:8888/gemfire-api/v1/"}'
+
+Initialize Postgres database via:
+
+(cd $PROJECT_HOME/scripts; PGPASSWORD=pivotal psql -h $DOCKER_MACHINE_IP -d gemfire -U pivotal -f model_postgres.sql)
 EOF
